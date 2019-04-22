e261aaf2e0a49caeecb2d112ab295e554b88a4b2
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2019-04-22 12:48:15 -0500

    enemy path movement
    Path movement for enemy and locations with list. Still need to fix a few things

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] float enemyMoveSpeed = 2;
+
+    int waypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWayPoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMoveToPoint();
+    }
+
+    private void EnemyMoveToPoint()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var moveThisFrame = enemyMoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
